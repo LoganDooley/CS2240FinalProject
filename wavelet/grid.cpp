2806c6449362e07b4929967f32294d3e20a0c6e7
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2023-04-12 21:17:31 -0400

    started work on diffusion step

--- wavelet/grid.cpp
+++ wavelet/grid.cpp
@@ -1,6 +1,9 @@
 #include "grid.h"
-#include <math.h>
 #include <assert.h>
+#include <iterator>
+#include <math.h>
+
+Grid::Grid() : _resolution({0, 0, 0, 0}), _data() {}
 
 float &Grid::operator()(int xIndex, int yIndex, int kIndex, int thetaIndex) {
     // compile these out of release for efficiency
@@ -9,28 +12,103 @@ float &Grid::operator()(int xIndex, int yIndex, int kIndex, int thetaIndex) {
     assert(kIndex >= 0 && kIndex < _resolution[2]);
     assert(thetaIndex >= 0 && thetaIndex < _resolution[3]);
 
-    return _data[xIndex + yIndex * _resolution[0] + kIndex * _resolution[1] * _resolution[0] + 
+    return _data[xIndex + yIndex * _resolution[0] +
+        kIndex * _resolution[1] * _resolution[0] +
         thetaIndex * _resolution[0] * _resolution[1] * _resolution[2]];
 }
 
-float const &Grid::operator()(int xIndex, int yIndex, int kIndex, int thetaIndex) const {
+float const &Grid::operator()(int xIndex, int yIndex, int kIndex,
+        int thetaIndex) const {
     // compile these out of release for efficiency
     assert(xIndex >= 0 && xIndex < _resolution[0]);
     assert(yIndex >= 0 && yIndex < _resolution[1]);
     assert(kIndex >= 0 && kIndex < _resolution[2]);
     assert(thetaIndex >= 0 && thetaIndex < _resolution[3]);
 
-    return _data[xIndex + yIndex * _resolution[0] + kIndex * _resolution[1] * _resolution[0] + 
+    return _data[xIndex + yIndex * _resolution[0] +
+        kIndex * _resolution[1] * _resolution[0] +
         thetaIndex * _resolution[0] * _resolution[1] * _resolution[2]];
 }
 
-float WaveletGrid::angularFrequency(float wavenumber) {
-    return sqrt(wavenumber * gravity + surfaceTension * wavenumber * wavenumber * wavenumber);
+int Grid::getResolution(int dimension) { return _resolution[dimension]; }
+
+void Grid::resize(std::array<int, 4> newResolution) {
+    _resolution = newResolution;
+    _data.resize(_resolution[0] * _resolution[1] * _resolution[2] *
+            _resolution[3]);
 }
 
-float WaveletGrid::groupSpeed(float wavenumber) {
+// wavelet grid
+
+WaveletGrid::WaveletGrid(std::array<int, 4> resolution)
+    : _minParam(-size, -size, 0, minZeta), _maxParam(size, size, tau, maxZeta),
+    _resolution(resolution) {
+
+        glm::vec4 resolutionVec(resolution[0], resolution[1], resolution[2],
+                resolution[3]);
+        _pixelParam = (_maxParam - _minParam) / resolutionVec;
+    }
+
+float WaveletGrid::angularFrequency(float wavenumber) {
+    return sqrt(wavenumber * gravity +
+            surfaceTension * wavenumber * wavenumber * wavenumber);
+}
+
+float WaveletGrid::advectionSpeed(float wavenumber) {
     float numerator = (gravity + 3 * surfaceTension * wavenumber * wavenumber);
     float denominator = 2 * angularFrequency(wavenumber);
     assert(denominator);
     return numerator / denominator;
+}
+
+float WaveletGrid::dispersionSpeed(float wavenumber) {
+    // courtesy of wolfram alpha 
+    // https://www.wolframalpha.com/input?i=d%5E2%2Fdx%5E2%28sqrt%28ax%2Bbx%5E3%29%29
+    float numerator =
+        (-2 * gravity + 6 * gravity * surfaceTension * wavenumber * wavenumber +
+         3 * surfaceTension * surfaceTension * wavenumber * wavenumber * wavenumber * wavenumber);
+    float denom = 4 * pow(wavenumber * (gravity + surfaceTension * wavenumber * wavenumber), 3 / 2);
+    assert(denom);
+    return numerator / denom;
+}
+
+void WaveletGrid::diffusionStep(float deltaTime) {
+    float spacialResolution = _pixelParam[X] * _pixelParam[Y];
+
+    for (int i_x = 0; i_x < amplitudes.getResolution(X); i_x++) {
+        for (int i_y = 0; i_y < amplitudes.getResolution(Y); i_y++) {
+            for (int i_theta = 0; i_theta < amplitudes.getResolution(THETA);
+                    i_theta++) {
+                for (int i_k = 0; i_k < amplitudes.getResolution(K); i_k++) {
+                    glm::vec4 pos = getPositionAtIndex({i_x, i_y, i_theta, i_k});
+                    float wavenumber = pos[K];
+
+                    // found on bottom of page 6
+                    float delta = 1e-5 * spacialResolution * spacialResolution *
+                        (_pixelParam[K] * _pixelParam[K]) * dispersionSpeed(wavenumber);
+
+                    // found on bottom of page 6
+                    float gamma = 0.025 * advectionSpeed(wavenumber) * _pixelParam[THETA] *
+                        _pixelParam[THETA] / spacialResolution;
+
+                    // TODO: write code to determine if a position is at least 2 away from
+                    // boundary
+                    bool atLeast2AwayFromBoundary = true;
+
+                    if (atLeast2AwayFromBoundary) {
+                    } else {
+                    }
+
+                    amplitudes_nxt(i_x, i_y, i_theta, i_k) =
+                        amplitudes(i_x, i_y, i_theta, i_k);
+                }
+            }
+        }
+    }
+    std::swap(amplitudes, amplitudes_nxt);
+}
+
+glm::vec4 WaveletGrid::getPositionAtIndex(std::array<int, 4> index) {
+    glm::vec4 indexVec(index[0], index[1], index[2], index[3]);
+    return _minParam + (indexVec + glm::vec4(0.5)) * _pixelParam;
 }
--- wavelet/grid.h
+++ wavelet/grid.h
@@ -2,10 +2,15 @@
 
 #include <array>
 #include <vector>
+#include <cmath>
+#include <glm/vec4.hpp>
 
 class Grid {
     public: 
         Grid();
+
+        int getResolution(int dimension);
+        void resize(std::array<int, 4> newResolution);
 
         float &operator()(int xIndex, int yIndex, int kIndex, int thetaIndex);
         float const &operator()(int xIndex, int yIndex, int kIndex, int thetaIndex) const;
@@ -15,15 +20,44 @@ class Grid {
 };
 
 class WaveletGrid {
+        // this is pretty nice from the paper's implementation
+        const int X = 0, Y = 1, THETA = 2, K = 3;
+
+        constexpr static float tau = 6.28318530718f;
         const float gravity = 9.81;
         const float surfaceTension = 72.8 / 1000; // surface tension of water
+        const float minZeta = log(0.03) / log(2);
+        const float maxZeta = log(10) / log(2);
+        const float size = 50;
+    private:
+        std::array<int,4> _resolution;
+        glm::vec4   _minParam,
+                    _maxParam,
+                    _pixelParam;
+
     public:
-        WaveletGrid();
+        /**
+         * Create a new wavelet grid, with the specified resolution.
+         * The first two values in the resolution represents the spacial resolution
+         * for x and y.
+         * The last two values represents the frequency resolution, in terms of theta and
+         * the wavenumber.
+         */
+        WaveletGrid(std::array<int, 4> resolution);
 
         void takeStep(float dt);
         void heightFieldEvaluation(); // please change parameters to this
     private:
         Grid amplitudes;
+        Grid amplitudes_nxt;
+
+        /**
+         * Obtains a position, in spacial x frequency space, with the specified 
+         * table index.
+         *
+         * @param index the specified index.
+         */
+        glm::vec4 getPositionAtIndex(std::array<int,4> index);
 
         void advectionStep(float dt); // see section 4.2 of paper
         void diffusionStep(float dt); // see section 4.2 of paper
@@ -38,12 +72,21 @@ class WaveletGrid {
         float angularFrequency(float wavenumber);
 
         /**
-         * @brief Computes the group speed. This is essentially the derivative of angular
+         * @brief Computes the group speed / advection speed. This is essentially the derivative of angular
          * frequency with respect to the wavenumber.
          *
          * @param wavenumber this is k in the paper, which is the length of the wavevector bold k
          *
-         * see equation 18 in the paper. We use this to compute the diffusion amount
+         * see equation 18 in the paper. We use this to compute the diffusion amount.
          */
-        float groupSpeed(float wavenumber);
+        float advectionSpeed(float wavenumber);
+
+        /**
+         * @brief Computes the dispersion speed. This is the derivative of the advection speed.
+         *
+         * @param wavenumber this is k in the paper, which is the length of the wavevector bold k
+         *
+         * see equation 18 in the paper. We use this to compute the diffusion amount.
+         */
+        float dispersionSpeed(float wavenumber);
 };
