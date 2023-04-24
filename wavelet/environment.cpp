0945270e57ec043dd2e5f83cf9c21f0be57c2fe7
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2023-04-24 04:46:56 -0400

    temp box implementation

--- wavelet/environment.cpp
+++ wavelet/environment.cpp
@@ -1,13 +1,24 @@
 #include "wavelet/environment.h"
 
-bool Environment::inDomain(glm::vec2 pos) const {
-    return levelSet(pos) >= 0;
-}
+bool Environment::inDomain(glm::vec2 pos) const { return levelSet(pos) >= 0; }
 
+// make a box with flat sides
 float Environment::levelSet(glm::vec2 pos) const {
+  if (pos.x < 1 && pos.x > -1 && pos.y < 1 && pos.y > -1) {
+    return 1;
+  } else if (pos.x == 1 || pos.x == -1 || pos.y == 1 || pos.y == -1) {
     return 0;
+  } else {
+    return -1;
+  }
 }
 
 glm::vec2 Environment::levelSetGradient(glm::vec2 pos) const {
-    return glm::vec2(0,0);
+  if (pos.x < 1 && pos.x > -1 && pos.y < 1 && pos.y > -1) {
+    return glm::vec2(0, 0);
+  } else if (pos.x == 1 || pos.x == -1 || pos.y == 1 || pos.y == -1) {
+    return glm::vec2(FLT_MAX, FLT_MAX);
+  } else {
+    return -1;
+  }
 }
--- wavelet/environment.h
+++ wavelet/environment.h
@@ -3,10 +3,10 @@
 #include "glm/vec2.hpp"
 
 class Environment {
-    public:
-        bool inDomain(glm::vec2 pos) const;
-        // should be distance from boundary
-        float levelSet(glm::vec2 pos) const;
-        // slope
-        glm::vec2 levelSetGradient(glm::vec2 pos) const;
+public:
+  bool inDomain(glm::vec2 pos) const;
+  // should be distance from boundary
+  float levelSet(glm::vec2 pos) const;
+  // slope
+  glm::vec2 levelSetGradient(glm::vec2 pos) const;
 };
