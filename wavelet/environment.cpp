f6c7fe967eab6e4bb14b4d0d45426f4dd5586f70
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2023-04-24 23:01:08 -0400

    implementation of environment with heightmaps

--- wavelet/environment.cpp
+++ wavelet/environment.cpp
@@ -1,25 +1,50 @@
-#include "wavelet/environment.h"
+#include "environment.h"
 #include <cfloat>
+
+Environment::Environment(std::string filename, float zBoundary) {
+  int width, height, n;
+  unsigned char *data = stbi_load(filename.c_str(), &width, &height, &n, 0);
+  if (data == NULL) {
+    std::cout << "Error loading image" << std::endl;
+    exit(1);
+  }
+  heights.resize(width);
+  gradients.resize(width);
+  for (int i = 0; i < width; i++) {
+    heights[i].resize(height);
+    gradients[i].resize(height);
+    for (int j = 0; j < height; j++) {
+      int index = (i + j * width) * n;
+      float r = data[index] / 255.0f;
+      float g = data[index + 1] / 255.0f;
+      float b = data[index + 2] / 255.0f;
+      float height = (r + g + b) / 3.0f;
+      heights[i][j] = height;
+      gradients[i][j] = glm::vec2(0, 0);
+    }
+  }
+  for (int i = 1; i < width - 1; i++) {
+    for (int j = 1; j < height - 1; j++) {
+      float dx = heights[i + 1][j] - heights[i - 1][j];
+      float dy = heights[i][j + 1] - heights[i][j - 1];
+      gradients[i][j] = glm::normalize(glm::vec2(dx, dy));
+    }
+  }
+  stbi_image_free(data);
+  this->zBoundary = zBoundary;
+}
 
 bool Environment::inDomain(glm::vec2 pos) const { return levelSet(pos) >= 0; }
 
-// make a box with flat sides
 float Environment::levelSet(glm::vec2 pos) const {
-  if (pos.x < 1 && pos.x > -1 && pos.y < 1 && pos.y > -1) {
-    return 1;
-  } else if (pos.x == 1 || pos.x == -1 || pos.y == 1 || pos.y == -1) {
-    return 0;
+  float zVal = heights[(int)((pos.x))][(int)((pos.y))];
+  if (zVal < zBoundary) {
+    return zBoundary - zVal;
   } else {
     return -1;
   }
 }
 
 glm::vec2 Environment::levelSetGradient(glm::vec2 pos) const {
-  if (pos.x < 1 && pos.x > -1 && pos.y < 1 && pos.y > -1) {
-    return glm::vec2(0, 0);
-  } else if (pos.x == 1 || pos.x == -1 || pos.y == 1 || pos.y == -1) {
-    return glm::vec2(FLT_MAX, FLT_MAX);
-  } else {
-    return glm::vec2(-1, -1);
-  }
+  return gradients[(int)((pos.x))][(int)((pos.y))];
 }
--- wavelet/environment.h
+++ wavelet/environment.h
@@ -1,12 +1,31 @@
 #pragma once
 
+#include "External/stb/stb_image.h"
+#include "glm/glm.hpp"
 #include "glm/vec2.hpp"
+#include <iostream>
+#include <vector>
 
 class Environment {
 public:
+  Environment() = default;
+
+  /**
+   * @brief Construct the environment
+   *
+   * @param filename = corresponding to the image heightmap which will be used
+   * for the environment
+   * @param zBoundary = the z value under which we should simulate
+   */
+  Environment(std::string filename, float zBoundary);
   bool inDomain(glm::vec2 pos) const;
   // should be distance from boundary
   float levelSet(glm::vec2 pos) const;
   // slope
   glm::vec2 levelSetGradient(glm::vec2 pos) const;
+
+private:
+  float zBoundary;
+  std::vector<std::vector<float>> heights;
+  std::vector<std::vector<glm::vec2>> gradients;
 };
